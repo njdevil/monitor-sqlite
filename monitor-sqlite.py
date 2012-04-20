600d57e34750add8db5959e7a6b636be5ac20fe9
Matt Lynch <njdevil@gmail.com>
2012-04-20 12:06:20 -0400

    Initial Sqlite3 version

---
+++ monitor-sqlite.py
@@ -0,0 +1,71 @@
+import datetime
+import commands
+import sqlite3
+import os
+import re
+
+def checkdb():
+    if not os.path.exists('/root/monitor.db'):
+        dbconn=sqlite3.connect('/root/monitor.db')
+        query=dbconn.cursor()
+        query.execute('create table records (command text, date real, time real, value real)')
+        dbconn.commit()
+        dbconn.close()
+
+
+def dates():
+    date=(datetime.datetime.now()).strftime("%Y%m%d")
+    time=(datetime.datetime.now()).strftime("%H%M")
+    return date,time
+
+
+def dbinsert(command,value,table='records'):
+    date,time=dates()
+    dbconn=sqlite3.connect('/root/monitor.db')
+    insert=dbconn.cursor()
+    insert.execute('insert into '+table+' values("'+command+',"'+date+'","'+time+'","'+value+'")')
+
+
+def getapachewc():
+    for a,b,c in os.walk('/var/log/apache2'):
+        for file in c:
+            data=""
+            if not re.search("mpsclient",a) and not re.search("alpha",a):
+                if re.search("access\.log$",file):
+                    data=commands.getoutput("wc -l "+os.path.join(a,file))
+                    data=re.sub(" .*$","",data)
+                    count.append(int(data))
+    dbinsert('wc',str(sum(count)))
+
+
+def getloadmem():
+    topdata=commands.getoutput("top -b -n 1")
+    topdatasplit=topdata.split("\n")
+    load=topdatasplit[0]
+    memory=topdatasplit[3]
+    processload(load)
+    processmem(memory)
+
+
+def processload(load):
+    load=re.sub("^.*load average: ","",load)
+    load=re.sub(",.*","",load)
+    load=float(load)
+    load=str(load*100)
+    dbinsert('load',load)
+
+
+def processmem(memory):
+    memory=re.sub("^.*total,","",memory)
+    memory=re.sub("k used,.*","",memory)
+    memory=re.sub("\s","",memory)
+    memory=float(memory)
+    memory=str(round(memory/1000,0))
+    dbinsert('memory',memory)
+
+
+if __name__=="__main__":
+    checkdb()
+    getloadmem()
+    getapachewc()
+    
